f5e38e6f65afd9b91c1be79e967aa782e78a2169
Franck LEBAS <franck.lebas@pocketlaw.com>
2021-10-10 10:09:49 +0200

    feat: next configuration

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import { Layout } from "../components/layout"
+import { Nav } from "../components/nav"
+
+const Index = () => {
+  return (
+    <Layout>
+      <Nav />
+    </Layout>
+  )
+}
+
+export default Index
