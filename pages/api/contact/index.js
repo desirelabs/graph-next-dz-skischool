68f8471f367c9010e08015132d2d72566d8a2d7d
Franck LEBAS <francklebas@protonmail.ch>
2021-11-21 14:37:10 +0100

    feat: implement multiple recipient

--- pages/api/contact/index.js
+++ pages/api/contact/index.js
@@ -5,7 +5,10 @@ sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_PKEY);
 async function setMail(data) {
   const { name, email, message } = data.body;
   const msg = {
-    to: process.env.NEXT_PUBLIC_EMAIL_RECIPIENT,
+    to:
+      process.env.NEXT_PUBLIC_EMAIL_RECIPIENT.indexOf(",") !== -1
+        ? process.env.NEXT_PUBLIC_EMAIL_RECIPIENT.split(";")
+        : process.env.NEXT_PUBLIC_EMAIL_RECIPIENT,
     from: process.env.NEXT_PUBLIC_EMAIL_SENDER,
     subject: `[DZ-Skischool] message de ${email} - ${name}`,
     text: message,
